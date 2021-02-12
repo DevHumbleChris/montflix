d82619aa31b89691afa7259167f055225f756cc8
DevHumbleChris <63234437+DevHumbleChris@users.noreply.github.com>
2021-02-12 13:46:06 +0300

    Friday 12th Feb 2021 @ 01:45

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -4,15 +4,16 @@ const axios = require("axios")
 
 module.exports = {
     index: async (req, res) => {
-
+        let pageNo = Math.floor(Math.random() * 3)
+        if(pageNo == 0) pageNo = pageNo + 1; 
         try{
 
             // @ Get Popular On Theatres
-            const responseMoviesPopular = await axios.get(process.env.MOVIES_POPULAR)
+            const responseMoviesPopular = await axios.get(`${process.env.MOVIES_POPULAR}${pageNo}`)
             const popularMovies = responseMoviesPopular.data
     
             // @ Get Popular On Tv
-            const responseTvPopular = await axios.get(process.env.TV_POPULAR)
+            const responseTvPopular = await axios.get(`${process.env.TV_POPULAR}${pageNo}`)
             const popularOnTv = responseTvPopular.data
     
             res.render("index",{
@@ -21,6 +22,7 @@ module.exports = {
             })
         }catch(err){
             console.log("An Error Occurred While Fetching Data.")
+            console.log(err.message)
         }
 
     },
@@ -40,17 +42,29 @@ module.exports = {
             const responseMovieCredits = await axios.get(`${process.env.MOVIE_DETAIL_URL}${id}${process.env.MOVIE_DETAIL_CREDITS}`)
             const movieCredits = responseMovieCredits.data
 
+            // @ Get YouTube Videos Key.
+            const responseYouTubeKeys = await axios.get(`${process.env.VIDEO_DB_URL}${id}${process.env.VIDEOS_REST_URL}`)
+            const youTubeKeys = responseYouTubeKeys.data
+
+            const results = youTubeKeys.results
+            let key = []
+            for( let x = 0; x < results.length; x++){
+                if(results[x].type == "Trailer"){
+                    key.push(results[x].key)
+                }
+            }
             res.render("movie_detail",{
                 movieDetails,
                 similarMovies,
-                movieCredits
+                movieCredits,
+                youTubeKey: key[0] ?? results.key
             })
+            console.log(results.key)
 
         }catch(err){
             console.log("Error Occurred While Fetching The Data")
+            console.log(err.message)
         }
-
-        console.log(`${process.env.MOVIE_DETAIL_URL}${id}${process.env.MOVIE_DETAIL_REST_URL}`)
     },
     tvDetail: (req, res) => {
         const id = req.params.id
