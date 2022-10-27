b91fbfcd7509d6490d59c353d0561ce00ccc5864
DevHumbleChris <dev.humblechris254@gmail.com>
2022-10-27 12:08:22 +0300

    Added vercel

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ const PORT = process.env.PORT ?? 3000
 const mainRoutes = require("./routes/mainRoutes")
 const mainControllers = require("./controllers/mainControllers")
 
-// @ Middlewares. 
+// @ Middlewares.
 app.use("/", mainRoutes)
 app.use(morgan("tiny"))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 app.use(express.static("public"))
 
-// @ Error Handlers. 
+// @ Error Handlers.
 app.use(mainControllers.error404)
 app.use(mainControllers.error500)
 
 app.listen(PORT, () => {
     console.log(`Montflix Started at http://127.0.0.1:${PORT}`.bold.blue)
 })
+
+module.exports = {
+    app
+}
